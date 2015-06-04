1f0a8348c121c3dffa084e77952fd6a34e170605
Jasonspd <jasoncluu@googlemail.com>
2015-06-04 06:24:20 +0100

    simple state

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,10 +1,36 @@
 var React = require("react");
+var Store = require("../stores/Store");
+
+function getStateFromStore() {
+	return {
+		data: Store.getData()
+	};
+};
 
 var MainApp = React.createClass({
+
+	getInitialState: function() {
+		return getStateFromStore();
+	},
+
+	componentDidMount: function() {
+		Store.addChangeListener(this._onChange);
+	},
+
+	componentWillUnmount: function() {
+		Store.removeChangeListener(this._onChange);
+	},
+
+	_onChange: function() {
+		this.setState(getStateFromStore());
+	},
+
 	render: function() {
+		console.log(this.state.data);
 		return (
 			<div className="app-wrapper">
-				<h1>It is React</h1>
+				<h1>My name is {this.state.data.name}</h1>
+				<h3>I am {this.state.data.age} years old</h3>
 			</div>
 		);
 	}
--- src/stores/Store.js
+++ src/stores/Store.js
@@ -6,18 +6,24 @@ var Assign 			= require("object-assign");
 
 var CHANGE_EVENT = "change";
 
+var _data = { name: "Jason", age: 23};
+
 var Store = Assign({}, EventEmitter.prototype, {
 
 	emitChange: function() {
 		this.emit(CHANGE_EVENT);
 	},
 
-	addChangeLister: function(callback) {
+	addChangeListener: function(callback) {
 		this.on(CHANGE_EVENT, callback);
 	},
 
 	removeChangeListener: function(callback) {
 		this.removeListener(CHANGE_EVENT, callback);
+	},
+
+	getData: function() {
+		return _data;
 	}
 });
 
